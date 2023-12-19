deab663e2d44a4f5b1537a4cd1217b75045f5b7c
RemiHiya <remsch13@gmail.com>
2023-12-19 13:48:59 +0100

    Collision data generation implemented

--- src/main/kotlin/elements/Physics.kt
+++ src/main/kotlin/elements/Physics.kt
@@ -1,5 +1,6 @@
 package elements
 
+import api.math.rotate
 import api.math.rotation
 import api.math.times
 import api.math.transformBy
@@ -9,11 +10,17 @@ import utils.Vector4
 
 class Physics {
     companion object {
-        private var objects: MutableList<PhysicsComponent> = mutableListOf()
-        private var potentialColliding: MutableSet<Pair<PhysicsComponent, PhysicsComponent>> = mutableSetOf()
-        private var colliding: MutableSet<Pair<
-                Pair<PhysicsComponent, PhysicsComponent>,
-                Pair<Vector4, Float>>> = mutableSetOf()
+        /**
+         * Structure prévue pour encapsuler les données de collision entre 2 objets.
+         * @property location Le centre du volume d'intersection.
+         * @property distance La taille maximale du volume d'intersection.
+         * @property normals Les normales de collision respectivement sur les 2 objets.
+         */
+        data class CollisionData(val location: Vector4, val distance: Float, val normals: Pair<Vector4, Vector4>)
+
+        private val objects = mutableListOf<PhysicsComponent>()
+        private val potentialColliding = mutableSetOf<Pair<PhysicsComponent, PhysicsComponent>>()
+        private val colliding = mutableSetOf<Triple<PhysicsComponent, PhysicsComponent, CollisionData>>()
 
         // TODO : pas de collision response entre 2 objets statiques
 
@@ -68,31 +75,68 @@ class Physics {
         }
 
         private fun detect() {
-            // TODO : remplir le set "colliding" avec les collisions data
+            colliding.clear()
+            for (i in potentialColliding) {
+                if (i.first.type == ColliderType.STATIC && i.second.type == ColliderType.STATIC)
+                    continue
+                val data = collisionData(i.first to i.second)
+                if (data.distance <= 0) {
+                    colliding += Triple(i.first, i.second, data)
+                }
+            }
         }
 
-        private fun collisionData(objects: Pair<PhysicsComponent, PhysicsComponent>): Pair<Vector4, Float> {
-            var closestPoints: Pair<Vector4, Float> = Pair(Vector4(), 0f)
-            var minDistance = Float.MAX_VALUE
+        /**
+         * Génère les données de collision entre 2 objets.
+         * @param objects La paire d'objets sur laquelle générer des données.
+         * @return Les données de collision, voir [CollisionData].
+         */
+        private fun collisionData(objects: Pair<PhysicsComponent, PhysicsComponent>): CollisionData {
             val a = objects.first
             val b = objects.second
+            var distance = Float.MAX_VALUE
 
             val other = if (a.points.size < b.points.size) b else a
             val self = if (a.points.size < b.points.size) a else b
             val points = self.points.map { (it.first+self.center) to it.second }
             val distFun = other.description
 
+            // Listes permettant de faire la moyenne des points en collision
+            val locations = mutableListOf<Vector4>()
+            // Stoque les positions locales pour éviter d'avoir à transformer les points après la boucle
+            val selfLocations = mutableListOf<Vector4>()
+            val otherLocations = mutableListOf<Vector4>()
+
             for (p in points) {
-                val worldPoint = p.first.transformBy(self.parent.transform)
+                val worldPoint = (p.first*.5f).transformBy(self.parent.transform)
                 val toOther = rotation(other.parent.transform.rotation.toRadians()).inverse() * (worldPoint - other.parent.transform.location)
-                val tmp = distFun(toOther) + 1f
-                if (tmp < minDistance) {
-                    minDistance = tmp
-                    closestPoints = worldPoint to tmp
+                val dist = distFun(toOther)
+                if (dist <= 0) {
+                    locations.add(worldPoint)
+                    selfLocations.add(p.first)
+                    otherLocations.add(toOther)
+                    if (dist < distance) {
+                        distance = dist
+                    }
                 }
             }
-            return closestPoints
+
+            var result = CollisionData(
+                locations.reduce(Vector4::plus)/locations.size.toFloat(),
+                distance,
+                Vector4() to Vector4()
+            )
+            selfLocations.reduce(Vector4::plus)/selfLocations.size.toFloat()
+            otherLocations.reduce(Vector4::plus)/otherLocations.size.toFloat()
+
+            if (result.distance <= 0) {
+                val normalA = other.getNormal(otherLocations.reduce(Vector4::plus)/otherLocations.size.toFloat()).rotate(other.parent.transform.rotation)
+                val normalB = self.getNormal(selfLocations.reduce(Vector4::plus)/selfLocations.size.toFloat()).rotate(self.parent.transform.rotation)
+                result = result.copy(normals = normalA to normalB)
+            }
+            return result
         }
+
 
         private fun resolve() {
         }
--- src/main/kotlin/utils/Structs.kt
+++ src/main/kotlin/utils/Structs.kt
@@ -1,5 +1,3 @@
 package utils
 
 data class MarcherOperator(var operator: OperatorType, var smoothness: Float)
-
-data class MarcherObjetct(var index: Int, var operator: MarcherOperator)
